4e94e272561e39627da7a92cb411671fac781703
Mille737 <mille737@gmail.com>
2020-02-21 08:38:03 +0100

    Initial commit

---
+++ src/Maze.java
@@ -0,0 +1,8 @@
+import java.util.LinkedList;
+
+public class Maze {
+
+    public int [][] maze;
+    LinkedList<Position> path = new LinkedList<>();
+    public Position start;
+}
---
+++ src/Position.java
@@ -0,0 +1,9 @@
+public class Position {
+    int x;
+    int y;
+
+    public Position(int y, int x) {
+        this.y = y;
+        this.x = x;
+    }
+}
